97cda4004f551ea0590d19419b603121ce64d7be
srijani01 <srijani.santra18@siesgst.ac.in>
2022-07-31 17:35:09 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainpageComponent } from './mainpage/mainpage.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AdminComponent } from './admin/admin.component';
+import { ProductinfoComponent } from './productinfo/productinfo.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { BillComponent } from './bill/bill.component';
+import { UserloginComponent } from './userlogin/userlogin.component';
+import { IndexComponent } from './index/index.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'index',pathMatch: 'full'},
+  {path:'product-list', component: ProductListComponent},
+  {path:'register', component: RegisterComponent},
+  {path:'mainpage', component: MainpageComponent},
+  {path:'dashboard',component:DashboardComponent},
+  {path:'admin', component:AdminComponent},
+  {path:'productinfo', component:ProductinfoComponent},
+  {path:'checkout', component:CheckoutComponent},
+  {path:'bill', component:BillComponent},
+  {path:'userlogin', component:UserloginComponent},
+  {path:'index', component:IndexComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { AdminComponent } from './admin/admin.component';
+import { RegisterComponent } from './register/register.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProductinfoComponent } from './productinfo/productinfo.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { BillComponent } from './bill/bill.component';
+import { UserloginComponent } from './userlogin/userlogin.component';
+import { IndexComponent } from './index/index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    AdminComponent,
+    RegisterComponent,
+    MainpageComponent,
+    DashboardComponent,
+    ProductinfoComponent,
+    CheckoutComponent,
+    BillComponent,
+    UserloginComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
