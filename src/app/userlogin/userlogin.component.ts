a97303b2106afb9aa68b49e51aeb906632be3cba
srijani01 <srijani.santra18@siesgst.ac.in>
2022-08-01 09:45:14 +0530

    third commit

--- src/app/userlogin/userlogin.component.ts
+++ src/app/userlogin/userlogin.component.ts
@@ -8,23 +8,23 @@ import { RegistrationService } from '../registration.service';
   styleUrls: ['./userlogin.component.css']
 })
 export class UserloginComponent implements OnInit {
-userName:string=''
-password:string=''
-  constructor(private router:Router,private rs:RegistrationService) { }
+// userName:string=''
+// password:string=''
+  constructor() { }
 
   ngOnInit(): void {
   }
-  login(user:string,pass:string){
-    this.userName=user;
-    this.password=pass;
-    if(this.userName=='admin'&& this.password=='admin')
-    this.router.navigate(['userlogin/',this.userName]);
-    else{
-      console.log(this.userName+" "+this.password)
-      //this.rs.userlogin(this.userName,this.password).subscribe(()=>{
-        //this.router.navigate(['userlogin/:user',this.userName])
-      //})
-    }
-  }
+  //login(user:string,pass:string){
+    // this.userName=user;
+    // this.password=pass;
+    // if(this.userName=='admin'&& this.password=='admin')
+    // this.router.navigate(['userlogin/',this.userName]);
+    // else{
+    //   console.log(this.userName+" "+this.password)
+    //    this.rs.userlogin(this.userName,this.password).subscribe(()=>{
+    //     this.router.navigate(['userlogin/:user',this.userName])
+    //    })
+    // }
+  //}
 
 }
